e02025f882ef00d06b425fb804d50c4f30927f66
filippodaniotti <febbrosto@gmail.com>
2021-01-24 01:08:33 +0100

    Heroku Redis setup

--- src/handlers.py
+++ src/handlers.py
@@ -1,6 +1,6 @@
 import io
 import os
-# import redis
+import redis
 import random
 import logging
 from datetime import date, time
@@ -16,21 +16,29 @@ REF_DATE = date(2017, 10, 22)
 SEND_TIME = time(7, 30, 0) # should be 8:30 but timezones suck
 help_message = 'El bot uficiałe de quei che ghe piaxe el Doxe del Veneto'
 
+r = redis.from_url(os.get.envrion("REDIS_URL"))
+db_keys = r.keys(pattern="*")
+
 # Hardcoded stuff, definitely to improve
 with io.open('./assets/quotes.txt', 'r', encoding='utf8') as fquotes:
     quotes = fquotes.read().split('\n')
 fquotes.close()
 
-# r = redis.from_url(os.get.envrion("REDIS_URL"))
-
 # Functions, commands, handler definitions
 def start(update, context):
-    # user_id = update.message.chat_id
-    # user_name = update.message.from_user.name
-    # r.set (user_name, user_id)
-    # print(update.effective_chat.id)
+    user_id = update.message.chat_id
+    user_name = update.message.from_user.name
+    r.set(user_name, user_id)
     context.bot.send_message(chat_id=update.effective_chat.id, text=help_message)
-    # context.job_queue.run_daily(callback_img, time=SEND_TIME, context=update.message.chat_id)
+    # context.job_queue.run_daily(callback_img, time=SEND_TIME)
+
+def subscribe(update, context):
+    user_id = update.message.chat_id
+    user_name = update.message.from_user.name
+    pass
+
+def unsubscribe(context, update):
+    pass
 
 def msg_handler(update, context):
     quote_trig(update, context)
@@ -50,21 +58,19 @@ def error(bot, update, error):
 #     text = f"Ennesimo rinvio par la autonomia, è una presa in giro: la misura è colma. Semo {str(days)} giorni in atesa del governo, can del porco!"
 #     context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
-# def callback_alarm(context):
-#     context.bot.send_message(chat_id=context.job.context, text='BEEP')
 
-def callback_img(context):
+# Jobs definition
+def daily_image(context):
     img_path = get_image()
     days = (date.today() - REF_DATE).days
     process_image(img_path, str(days))
+    for keys in db_keys:
+        keys_values = r.get(keys).decode("UTF-8")
+        print(keys_values)
+        context.bot.send_photo(chat_id=keys_values, photo=open(img_path, 'rb'))
     # context.bot.send_photo(chat_id=context.job.context, photo=open(img_path, 'rb'))
-    context.bot.send_photo(chat_id=os.environ.get('TO'), photo=open(img_path, 'rb'))
+    # context.bot.send_photo(chat_id=, photo=open(img_path, 'rb'))
     os.unlink(img_path)
-
-# def callback_timer(update, context):
-#     context.bot.send_message(chat_id=update.message.chat_id,
-#                              text='Setting a timer for 1 minute!')
-#     context.job_queue.run_daily(callback_img, time=time(21, 18, 30), context=update.message.chat_id)
 
 # Message Handlers definition
 def quote_trig(update, context):
